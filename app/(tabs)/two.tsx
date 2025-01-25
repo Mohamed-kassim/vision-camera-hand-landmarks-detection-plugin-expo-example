1a294fb90ec1865f13815361ec23b6195b60dcb2
MohamedKassim <mohamedkassim354@gmail.com>
2025-01-25 16:17:11 +0200

    Kickoff android

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,140 @@
-import { StyleSheet } from 'react-native';
+import React from "react";
+import {
+  Camera,
+  useCameraFormat,
+  useFrameProcessor,
+} from "react-native-vision-camera";
+import { useCameraPermission } from "react-native-vision-camera";
+import { useEffect, useState } from "react";
+import { useCameraDevice } from "react-native-vision-camera";
+import { StyleSheet, Text, useWindowDimensions, View } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { Worklets } from "react-native-worklets-core";
+import { detectHands, Hand } from "../../components/hand-detector";
 
 export default function TabTwoScreen() {
+  const device = useCameraDevice("back");
+  const format = useCameraFormat(device, [{ fps: 10 }]);
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+  console.log("dimensions", windowWidth, windowHeight);
+
+  const [hand, setHand] = useState<Hand | null>(null);
+  const handleHand = Worklets.createRunOnJS((detectedHand: Hand | null) => {
+    setHand(detectedHand);
+  });
+
+  // const frameProcessor = useFrameProcessor(
+  //   frame => {
+  //     'worklet';
+
+  //     // const frameWidth = frame.height;
+  //     // const frameHeight = frame.width;
+  //     const hands = detectHands(frame, {
+  //       autoScale: true,
+  //       windowHeight: windowHeight,
+  //       windowWidth: windowWidth,
+  //     });
+  //     console.log('hands', hands);
+  //     // console.log('frameWidth, frameHeight', frameWidth, frameHeight);
+  //     // console.log('data', data);
+  //     // const scaleX = windowWidth / frameWidth;
+  //     // const scaleY = windowHeight / frameHeight;
+
+  //     const detectedHand = hands[0];
+  //     if (!detectedHand) {
+  //       handleHand(null);
+  //       return;
+  //     }
+  //     // Calculate scaling factor while preserving aspect ratio
+
+  //     // const points: Point[] = [];
+  //     // const normalizedPoints: Point[] = [];
+  //     // const jsScaledLandmarks: Point[] = [];
+  //     // for (const hand of data || []) {
+  //     //   for (const mark of hand.landmarks) {
+  //     //     // Rotate from landscape right to portrait by:
+  //     //     // - Swapping x and y coordinates
+  //     //     // - Inverting the new x coordinate (1 - x)
+  //     //     points.push({
+  //     //       x: (1 - mark.y) * frameWidth * scaleX,
+  //     //       y: mark.x * frameHeight * scaleY,
+  //     //     });
+  //     //     jsScaledLandmarks.push({
+  //     //       x: mark.x * frameWidth * scaleX,
+  //     //       y: mark.y * frameHeight * scaleY,
+  //     //     });
+  //     //   }
+  //     //   for (const mark of hand.normalizedLandmarks) {
+  //     //     normalizedPoints.push({
+  //     //       x: mark.y,
+  //     //       y: windowWidth - mark.x,
+  //     //     });
+  //     //   }
+  //     //   console.log('raw landmarks', hand.landmarks);
+  //     //   console.log('native scaled landmarks', hand.normalizedLandmarks);
+  //     //   console.log('js scaled landmarks', jsScaledLandmarks);
+  //     // }
+
+  //     // console.log('points', points);
+  //     // console.log('normalizedPoints', normalizedPoints);
+  //     console.log('detectedHand.handedness', detectedHand.handedness);
+  //     handleHand(detectedHand);
+  //   },
+  //   [windowWidth, windowHeight],
+  // );
+
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      "worklet";
+      const hands = detectHands(frame, {
+        autoScale: true,
+        windowHeight: windowHeight,
+        windowWidth: windowWidth,
+      });
+      console.log("hands", hands);
+      // console.log('hands', hands);
+      // const detectedHand = hands[0];
+      // if (!detectedHand) {
+      //   handleHand(null);
+      //   return;
+      // }
+      // console.log('detectedHand.handedness', detectedHand.handedness);
+      // handleHand(detectedHand);
+      // frame.close();
+    },
+    [windowWidth, windowHeight]
+  );
+
+  useEffect(() => {
+    requestPermission();
+  }, [requestPermission]);
+
+  if (!hasPermission) {
+    return <Text>No permission</Text>;
+  }
+  if (device == null) {
+    return <Text>No device</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+    <View style={StyleSheet.absoluteFill}>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+        frameProcessor={frameProcessor}
+        // fps={30}.
+
+        format={format}
+        pixelFormat="rgb"
+      />
+      {/* <HandLandmarks hand={hand} /> */}
     </View>
   );
 }
@@ -16,16 +142,16 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
---
+++ components/hand-detector/index.ts
@@ -0,0 +1,70 @@
+import {VisionCameraProxy, Frame} from 'react-native-vision-camera';
+const plugin = VisionCameraProxy.initFrameProcessorPlugin('detectHands', {});
+
+export interface HandDetectorOptions extends Record<string, any> {
+  /**
+   * The minimum confidence score for the hand detection to be considered successful.
+   * Defaults to 0.5. Must be between 0 and 1.
+   */
+  minHandDetectionConfidence?: number;
+  /**
+   * The minimum confidence score of hand presence score in the hand landmark detection.
+   * Defaults to 0.5. Must be between 0 and 1.
+   */
+  minHandPresenceConfidence?: number;
+  /**
+   * The minimum confidence score for the hand tracking to be considered successful.
+   * Defaults to 0.5. Must be between 0 and 1.
+   */
+  minTrackingConfidence?: number;
+  /**
+   * The maximum number of hands that can be detected by the model.
+   * Defaults to 1. Must be between 1 and 2.
+   */
+  numHands?: number;
+  /**
+   * Should auto scale face bounds, contour and landmarks on native side?
+   * This option should be disabled if you want to draw on frame using `Skia Frame Processor`.
+   * See [this](https://github.com/luicfrr/react-native-vision-camera-face-detector/issues/30#issuecomment-2058805546) and [this](https://github.com/luicfrr/react-native-vision-camera-face-detector/issues/35) for more details.
+   *
+   * @default false
+   */
+  autoScale?: boolean;
+  /**
+   * Required if you want to use `autoScale`. You must handle your own logic to get screen sizes, with or without statusbar size, etc...
+   *
+   * @default 1.0
+   */
+  windowWidth?: number;
+  /**
+   * Required if you want to use `autoScale`. You must handle your own logic to get screen sizes, with or without statusbar size, etc...
+   *
+   * @default 1.0
+   */
+  windowHeight?: number;
+}
+
+interface HandLandmark {
+  x: number;
+  y: number;
+}
+interface Handedness {
+  label: string;
+  score: number;
+  index: number;
+}
+interface Hand {
+  landmarks: HandLandmark[];
+  handedness: Handedness;
+}
+
+type Hands = Hand[];
+export function detectHands(frame: Frame, options: HandDetectorOptions): Hands {
+  'worklet';
+  if (plugin == null) {
+    throw new Error('Failed to load Frame Processor Plugin!');
+  }
+  return plugin.call(frame, options) as unknown as Hands;
+}
+
+export type {Hand, Hands};
